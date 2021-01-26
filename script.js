d44dee2f183323c834451580584dbdd792e0aeed
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-26 14:06:45 -0800

    Created a function so that time and seconds would update on screen.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//Added current date/time to top of page using moments.js
-$("#currentDay").text(moment().format("MMM Do YY"));
+// This function shows day/date/time with seconds changing using moments.js. Time displays in <p id="currentDay">.
+function clock() {
+    var changingTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $('#currentDay').html(changingTime);
+  }
+  
+  setInterval(clock, 1000);
+
+  
+//Add new row
+var newRow = $("<tr>");
+
+$(".conatiner").append(newRow);
+
+newRow.attr("class","row");
