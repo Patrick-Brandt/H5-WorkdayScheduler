3a7e8761aa5a67c424b9dfda53bc44086817dd43
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-31 16:28:27 -0800

    Restructured comments

--- script.js
+++ script.js
@@ -9,25 +9,25 @@ $(document).ready(function() {
     }
     
     setInterval(clock, 1000);
+
+    /*need to assign value to time block, or make it a var.
+    function colorChange() {
+    if time block <changingTime then 
+     $("textarea").addClass("past");
+     else if time block  = changingTime then
+     $("textarea").addClass("present");
+     else if time block > changingTime, then
+     $("textarea").addClass("future");*/
     
-     //Inside this function will be the color change function, using the preset classes provided.
 
-    //if time block < moments.js then 
-    // $("textarea").addClass("past");
-    // else if time block  = moments .js then
-    // $("textarea").addClass("present");
-    // else if time block > moments .js, then
-    // $("textarea").addClass("future");
-    
+    /*To-do: Save function- this function will stand on its own.
+       parse local storage
+    localStorage.setItem ("key","value");
+       for the code above maybe a timeblock name for the key, and value will be text from the text area?
+    var saveText = $(".saveBtn");*/
 
-    //To-do: Save function- this function will stand on its own.
-      // parse local storage
-    //localStorage.setItem ("key","value");
-      // for the code above maybe a timeblock name for the key, and value will be text from the text area?
-    //var saveText = $(".saveBtn");
-
-    //Do I create vars with strings that contain the text entered in text area? 
-    //Event objects will help me find out where things are occuring.
+    /*Do I create vars with strings that contain the text entered in text area? 
+      Event objects will help me find out where things are occuring.*/
     $("#9AM").on("click", function(event) {
       console.log(event.currentTarget);
   });
