d378e324b3ae82587aaf70aa0c393e78d7bcd2e1
Patrick Brandt <brandt.patrickc@gmail.com>
2021-02-02 14:07:03 -0800

    I removed divs that were used to test color changes now that color change is working, Commented out unneccesary console.logs, removed to-do list comments, and reformatted for cleanliness and appearance.

--- script.js
+++ script.js
@@ -11,36 +11,30 @@ $(document).ready(function() {
     }
     
     setInterval(clock, 1000);
+  });
 
     // This will loop through my time blocks.
     $(".time-block").each(function() {
       //console.log(this.id);
       var currentHourBlock = parseInt($(this).attr("hour"))
       //console.log("hour:", currentHourBlock);
-      //add conditional that adds 12 to any time between 1 and 5.
+      
       if (momentsBlock >= 1 && momentsBlock <= 5) {
-        momentsBlock += 12;
-        
+        momentsBlock += 12; 
       }
-      console.log(momentsBlock, currentHourBlock);
+      //console.log(momentsBlock, currentHourBlock);
 
+      //Conditional statement below changes classes on textarea depending on current time.
       if (momentsBlock > currentHourBlock) { 
         $(this).addClass("past");
         } else if (momentsBlock == currentHourBlock) {
         $(this).addClass("present");
         } else if (momentsBlock < currentHourBlock) {
         $(this).addClass("future");
-        };
-  
+      };
     })
 
-      //This function changes the color of the text area, using the "past, "present", and "future" css classes.
-      //Need to fix time-block
-      /*function colorChange() {
-      I had a conditional statement here, but moved it inside of my loop. Will keep the function colorChange here until I am certain that was the right move.
-      }*/
     
-
     /*To-do: Save function- this function will stand on its own.
     
     localStorage.setItem ("key","value");
@@ -50,7 +44,7 @@ $(document).ready(function() {
       //Event objects will help me find out where things are occuring.*/
     $("#9AM").on("click", function(event) {
       console.log(event.currentTarget);
-  });
+  
 
 }); 
 
