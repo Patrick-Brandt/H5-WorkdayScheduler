946156245d0929a90ae7e699a40159a3b361402a
Patrick Brandt <brandt.patrickc@gmail.com>
2021-02-02 18:16:17 -0800

    Added/changed comments throughout page. Added new divs to use as a test since I'm working outside of the daily planners business hours.

--- script.js
+++ script.js
@@ -9,47 +9,42 @@ $(document).ready(function() {
       var changingTime = moment().format('MMMM Do YYYY, h:mm:ss a');
       $('#currentDay').html(changingTime);
     }
-    
     setInterval(clock, 1000);
     render();
   });
 
-    // This will loop through my time blocks.
-    $(".time-block").each(function() {
-      //console.log(this.id);
-      var currentHourBlock = parseInt($(this).attr("hour"))
-      //console.log("hour:", currentHourBlock);
-      
-      if (momentsBlock >= 1 && momentsBlock <= 5) {
-        momentsBlock += 12; 
-      }
-      //console.log(momentsBlock, currentHourBlock);
+  // This will loop through my time blocks.
+  $(".time-block").each(function() {
+    /*console.log(this.id);  If I were working as a developer I would probably delete my console.logs once I was done 
+    with them, but since this is homework I will leave them so graders can see where/why I used them.*/
 
-      //Conditional statement below changes classes on textarea depending on current time.
-      if (momentsBlock > currentHourBlock) { 
-        $(this).addClass("past");
-        } else if (momentsBlock == currentHourBlock) {
-        $(this).addClass("present");
-        } else if (momentsBlock < currentHourBlock) {
-        $(this).addClass("future");
-      };
-    })
+    var currentHourBlock = parseInt($(this).attr("hour"))
+    //console.log("hour:", currentHourBlock);  
 
+    //Conditional statement below changes classes on textarea depending on current time.
+    if (momentsBlock > currentHourBlock) { 
+      $(this).addClass("past");
+      } else if (momentsBlock == currentHourBlock) {
+      $(this).addClass("present");
+      } else if (momentsBlock < currentHourBlock) {
+      $(this).addClass("future");
+    };
+  })
+    //This conditional statement is necessary so that I didn't have to convert clock/timeblocks to military time. 
+    if (momentsBlock >= 1 && momentsBlock <= 5) {
+      momentsBlock += 12; 
+    }
+    //console.log(momentsBlock, currentHourBlock);
     
-    /*To-do: Save function- this function will stand on its own.
-    
-    localStorage.setItem ("key","value");
-       for the code above maybe a timeblock name for the key, and value will be text from the text area?*/
-
+    // This function saves text area info to local storage.
       $(".saveBtn").click(function () {
         var saveText = $(this).siblings(".description").val();
         alert(saveText);
         var hourClass = $(this).siblings(".hour").text();
         localStorage.setItem(hourClass, JSON.stringify(saveText));
-        //there is a way to do this using 'this' to target each individual timeblock
       });
       
-      //render to screen-regular js function render. put inside of doc ready, but call it down here. look to quiz 
+      //render to screen-regular js function render. put inside of doc ready, but call it down here.look to quiz 
         function render (){
           
         }
